762f91c84ffb15b93c4bbcbffe2192c583133c7f
Jules <ju.pabst@gmail.com>
2023-12-11 12:42:02 +0100

    added print function

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define GENRES {"Horror", "Abenteuer", "Romantik", "Sachbuch" }
+
 typedef enum {
     horror, abenteuer, romantik, sachbuch
 } E_Genre;
@@ -18,6 +20,26 @@ typedef struct bookNode {
     struct bookNode* next;
 } bookNode;
 
+bookNode* createBookNode(char title[32], E_Genre genre, int publicationYear, int amount);
+
+bookNode* addBookNodeFront(bookNode* inventoryHead, bookNode* newNode, int* lengthInventory);
+
+void printInventory (bookNode* inventoryHead, int lengthInventory, char genres[4][32]);
+
+
+int main() {
+    char genres[4][32] = GENRES;
+    int lengthInventory = 0;
+
+    bookNode* inventoryHead = NULL;
+
+    inventoryHead = addBookNodeFront(inventoryHead, createBookNode("Huhu", horror, 1999, 2), &lengthInventory);
+
+    printInventory(inventoryHead, lengthInventory, genres);
+
+    return 0;
+}
+
 bookNode* createBookNode(char title[32], E_Genre genre, int publicationYear, int amount) {
     bookNode* newNode = malloc(sizeof(bookNode));
     printf("node allocated");
@@ -26,17 +48,24 @@ bookNode* createBookNode(char title[32], E_Genre genre, int publicationYear, int
     newNode->content.genre = genre;
     newNode->content.publicationYear = publicationYear;
     newNode->content.amount = amount;
+    newNode->next = NULL;
 
     return newNode;
 }
 
+bookNode* addBookNodeFront(bookNode* inventoryHead, bookNode* newNode, int* lengthInventory){
+    newNode->next = inventoryHead;
+    (*lengthInventory)++;
+    return newNode;
+}
 
-
-int main() {
-    char genres[4][32] = {"Horror", "Abenteuer", "Romantik", "Sachbuch" };
-    bookNode* inventory = NULL;
-
-    inventory = createBookNode("Huhu", horror, 1999, 2);
-
-    return 0;
+void printInventory (bookNode* inventoryHead, int lengthInventory, char genres[4][32] ){
+    bookNode* currentNode = inventoryHead;
+    for(int i = 0; i < lengthInventory; i++){
+        printf("\n%s", currentNode->content.title);
+        printf("\n%s", genres[currentNode->content.genre]);
+        printf("\n%d", currentNode->content.publicationYear);
+        printf("\n%d", currentNode->content.amount);
+        currentNode = currentNode->next;
+    }
 }
