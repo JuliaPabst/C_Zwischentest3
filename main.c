f713303fb7b2a0cdce6ff923b18f22acabbb77e5
Jules <ju.pabst@gmail.com>
2023-12-11 11:55:11 +0100

    added enum and struct

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+typedef enum {
+    horror, abenteuer, romantik, sachbuch
+} E_Genre;
+
+typedef struct {
+    char title[32];
+    E_Genre genre;
+    int erscheinungsjahr;
+    int anzahlExemplare;
+} S_Book;
+
 int main() {
-    
+
     return 0;
 }
