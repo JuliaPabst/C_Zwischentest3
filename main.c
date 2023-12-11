6f3bd9320289d8b4dedaac57f454954137e53245
Jules <ju.pabst@gmail.com>
2023-12-11 14:20:22 +0100

    added createBorrowNode and addBorrowNodeFront

--- main.c
+++ main.c
@@ -23,24 +23,41 @@ typedef struct bookNode {
     struct bookNode* next;
 } bookNode;
 
+typedef struct borrowNode {
+    char title[32];
+    char name[32];
+    struct borrowNode* next;
+} borrowNode;
+
 bookNode* createBookNode(char title[32], E_Genre genre, int publicationYear, int amount);
 
+borrowNode* createBorrowNode(int selectedBookToBorrow, char name[32], bookNode* inventoryHead);
+
 bookNode* addBookNodeFront(bookNode* inventoryHead, bookNode* newNode, int* lengthInventory);
+
+borrowNode* addBorrowNodeFront(borrowNode* booksBorrowedHead, borrowNode* newNode, int* lengthBorrowedNodes);
 
 void printInventory (bookNode* inventoryHead, int lengthInventory, char genres[4][32]);
 
 void freeInventory (bookNode* inventoryHead, int lengthInventory);
 
+void freeBorrowedNodes (borrowNode* booksBorrowedHead, int lengthBorrowedNodes);
+
 int main() {
     char genres[4][32] = GENRES;
     int lengthInventory = 0;
+    int lengthBorrowedNodes = 0;
     char selected = 'a';
+
     char title[32];
     int genre;
     int publicationYear = 0;
     int amount = 0;
+    int selectedBookToBorrow = 0;
+    char name[32];
 
     bookNode* inventoryHead = NULL;
+    borrowNode* booksBorrowedHead = NULL;
 
     while(selected != 'x'){
         printf("\nNeues Buch eingeben (n), Buch ausleihen (b), Buch zurueckgeben (r), Buecher auflisten (l), Buecher sortieren (s), Programm beenden (x)\n"
@@ -49,7 +66,7 @@ int main() {
         scanf(" %c", &selected);
 
         switch(selected){
-            case 'n':
+            case 'n': // insert book
                 printf("\nGeben Sie den Titel ein: ");
                 scanf(" %s", title);
                 while(genre < 1 || genre > 4){
@@ -73,22 +90,39 @@ int main() {
                         printf("\nUngueltige Eingabe!");
                     }
                 }
+                inventoryHead = addBookNodeFront(inventoryHead, createBookNode(title, genre, publicationYear, amount), &lengthInventory);
                 genre = 0;
                 publicationYear = 0;
                 amount = 0;
-                inventoryHead = addBookNodeFront(inventoryHead, createBookNode(title, genre, publicationYear, amount), &lengthInventory);
                 break;
-            case 'b':
+            case 'b': // borrow book
+                if (lengthInventory < 1){
+                    printf("\nEs sind keine Buecher im Inventar vorhanden.");
+                }
+                printInventory(inventoryHead, lengthInventory, genres);
+                while(selectedBookToBorrow < 1 || selectedBookToBorrow > lengthInventory){
+                    printf("\nWelchen Titel moechten Sie leihen? (1-%d): ", lengthInventory);
+                    scanf(" %d", &selectedBookToBorrow);
+                    if (selectedBookToBorrow < 1 || selectedBookToBorrow > lengthInventory) {
+                        printf("\nUngueltige Eingabe!");
+                    }
+                }
+
+                printf("\nGeben Sie Ihren Namen ein: ");
+                scanf(" %s", name);
+
+                booksBorrowedHead = addBorrowNodeFront(booksBorrowedHead, createBorrowNode(selectedBookToBorrow, name, inventoryHead), &lengthBorrowedNodes);
                 break;
-            case 'r':
+            case 'r': // return book
                 break;
-            case 'l':
+            case 'l': // list books
                 printInventory(inventoryHead, lengthInventory, genres);
                 break;
-            case 's':
+            case 's': // sort books
                 break;
             case 'x':
                 freeInventory (inventoryHead, lengthInventory);
+                freeBorrowedNodes (booksBorrowedHead, lengthBorrowedNodes);
                 break;
         }
     }
@@ -98,7 +132,7 @@ int main() {
 bookNode* createBookNode(char title[32], E_Genre genre, int publicationYear, int amount) {
     bookNode* newNode = malloc(sizeof(bookNode));
     #ifdef SHOWALLOCATIONS
-        printf("node allocated");
+        printf("book node allocated");
     #endif
     strcpy(newNode->content.title, title);
     newNode->content.genre = genre;
@@ -114,12 +148,37 @@ bookNode* addBookNodeFront(bookNode* inventoryHead, bookNode* newNode, int* leng
     return newNode;
 }
 
+borrowNode* createBorrowNode(int selectedBookToBorrow, char name[32], bookNode* inventoryHead){
+    borrowNode* newNode = malloc(sizeof(borrowNode));
+    bookNode* borrowedBook = inventoryHead;
+    #ifdef SHOWALLOCATIONS
+        printf("borrow node allocated");
+    #endif
+
+    for(int i = 0; i < selectedBookToBorrow; i++){
+        borrowedBook = borrowedBook->next;
+    }
+
+    strcpy(newNode->title, borrowedBook->content.title);
+    strcpy(newNode->name, name);
+    newNode->next = NULL;
+    return newNode;
+}
+
+borrowNode* addBorrowNodeFront(borrowNode* booksBorrowedHead, borrowNode* newNode, int* lengthBorrowedNodes){
+    newNode->next = booksBorrowedHead;
+    (*lengthBorrowedNodes)++;
+    return newNode;
+}
+
+
 void printInventory (bookNode* inventoryHead, int lengthInventory, char genres[4][32] ){
     bookNode* currentNode = inventoryHead;
     for(int i = 0; i < lengthInventory; i++){
         printf("\n%d: %s, %s, (%d)", i+1, currentNode->content.title, genres[currentNode->content.genre], currentNode->content.publicationYear);
         currentNode = currentNode->next;
     }
+    printf("\n");
 }
 
 void freeInventory (bookNode* inventoryHead, int lengthInventory){
@@ -130,6 +189,18 @@ void freeInventory (bookNode* inventoryHead, int lengthInventory){
         #ifdef SHOWFREEINGS
                 printf("\nNode freed");
         #endif
+        currentNode = nextNode;
+    }
+}
+
+void freeBorrowedNodes (borrowNode* booksBorrowedHead, int lengthBorrowedNodes){
+    borrowNode * currentNode = booksBorrowedHead;
+    for(int i = 0; i < lengthBorrowedNodes; i++){
+        borrowNode * nextNode = currentNode->next;
+        free(currentNode);
+    #ifdef SHOWFREEINGS
+            printf("\nNode freed");
+    #endif
         currentNode = nextNode;
     }
 }
